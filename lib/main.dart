645d4c62b1e21f23e3b36969e1f2027f96d8791b
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-07-26 15:21:42 +0200

    finishing the meal detail page

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,11 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName:(ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName:(ctx)=>MealDetailScreen(),
       },
+      onUnknownRoute: (settings){
+        return MaterialPageRoute(builder: (ctx)=>CategoriesScreen(),);
+
+
+      },
     );
   }
 }
